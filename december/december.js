521dd2a6ee7b2871410ddc72f5c28bf760d9f34b
Charlie Knudsen <charlie.knudsen@gmail.com>
2012-12-06 17:58:26 -0600

    Add some color

--- december/december.js
+++ december/december.js
@@ -1,18 +1,30 @@
 $(function() {
-    shakeTheTurkey();
+    colorHeadline();
 });
 
-function shakeTheTurkey() {
-    $('#turkey_pan').hide('shake', 1000, function() {
-        $('#turkey_pan').show(
-            'shake', 
-            1000, 
-            setTimeout('makeTheTurkeySayGobble();', 500)
-        );
-    });
+
+function colorHeadline() {
+    var updateSpeed = 400;
+    var holidayText = '';
+    var $message = $('#the_message');
+    var holidayText = $message.html();
+    var colorClasses = ['green', 'red'];
+
+    $message.html('');
+    for (var i = 0; i < holidayText.length; i++) {
+        var $newSpan = $('<span/>').html(holidayText[i]).addClass('uncolored').addClass(colorClasses[i % colorClasses.length]);
+        $message.append($newSpan);
+    }
+    var updateColors = function(){
+        var $letters = $('#the_message .uncolored');
+        var random_elem = Math.floor(Math.random() * $letters.length);
+        var $span = $letters.eq(random_elem);
+
+        $span.removeClass('uncolored');
+        if($letters.length > 0) {
+            setTimeout(updateColors, updateSpeed);
+        }
+    }
+    setTimeout(updateColors, updateSpeed);
 }
 
-function makeTheTurkeySayGobble() {
-    /* Todo... */
-    setTimeout('shakeTheTurkey();', 500)
-}
